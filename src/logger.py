9e0b82d73fd73bebb98c798a748c29f19c7fd6be
“Hruday <pamidi.hruday@gmail.com>
2025-02-13 23:59:19 -0600

    Logger file update

--- src/logger.py
+++ src/logger.py
@@ -1,17 +1,21 @@
+import os
 import logging
-import os 
 from datetime import datetime
 
+# Create the log file name based on the current date and time
 LOG_FILE = f"{datetime.now().strftime('%m_%d_%Y_%H_%M_%S')}.log"
-log_path = os.path.join(os.getcwd(), "logs", LOG_FILE)
-os.makedirs(log_path, exist_ok=True)
+# Set the path to the 'logs' directory under the current working directory
+log_dir = os.path.join(os.getcwd(), "logs")
+# Ensure the 'logs' directory exists
+os.makedirs(log_dir, exist_ok=True)
+# Create the full log file path
+LOG_FILE_PATH = os.path.join(log_dir, LOG_FILE)
 
-LOG_FILE_PATH = os.path.join(log_path, LOG_FILE)
-
+# Configure the logging
 logging.basicConfig(
     filename=LOG_FILE_PATH,
-    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s", 
-    level=logging.INFO,
+    format="[ %(asctime)s ] %(lineno)d %(name)s - %(levelname)s - %(message)s",
+    level=logging.INFO
 )
 
 '''
